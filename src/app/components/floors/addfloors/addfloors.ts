424787eef73390b7354ee10f7aaef8842625045e
Eiasin Prodhan <eiasinprodhan@gmail.com>
2025-07-12 15:47:00 +0600

    Added

--- src/app/components/floors/addfloors/addfloors.ts
+++ src/app/components/floors/addfloors/addfloors.ts
@@ -1,11 +1,62 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FloorService } from '../../../services/floor.service';
+import { BuildingService } from '../../../services/building.service';
 
 @Component({
   selector: 'app-addfloors',
   standalone: false,
   templateUrl: './addfloors.html',
-  styleUrl: './addfloors.css'
+  styleUrl: './addfloors.css',
 })
-export class Addfloors {
+export class Addfloors implements OnInit{
+  addFloorForm!: FormGroup;
+  buildings!: any;
+
+  message: string = '';
+  messageType: string = '';
+
+
+  constructor(
+    private floorService: FloorService,
+    private buildingService: BuildingService,
+    private formBuilder: FormBuilder,
+    private cdr: ChangeDetectorRef
+  ) {}
+
+  ngOnInit(): void {
+    this.addFloorForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      building: ['', Validators.required],
+    });
+
+    this.listBuildings();
+
+  }
+
+  addFloor(): void {
+    if (this.addFloorForm.invalid) {
+      this.addFloorForm.markAllAsTouched();
+      return;
+    }
+
+    this.floorService.addFloors(this.addFloorForm.value).subscribe({
+      next: () => {
+        this.message = 'Floor added successfully!';
+        this.messageType = 'success';
+        this.addFloorForm.reset();
+        this.cdr.detectChanges();
+      },
+      error: (err) => {
+        this.message = 'Failed to add floor. Please try again.';
+        this.messageType = 'danger';
+        console.error(err);
+      },
+    });
+  }
+
+  listBuildings(): void {
+    this.buildings = this.buildingService.listBuildings();
+  }
 
 }
--- src/app/components/floors/listfloors/listfloors.ts
+++ src/app/components/floors/listfloors/listfloors.ts
@@ -1,30 +1,50 @@
 import { ChangeDetectorRef, Component } from '@angular/core';
 import { FloorService } from '../../../services/floor.service';
 import { Router } from '@angular/router';
+import { BuildingService } from '../../../services/building.service';
+import { Building } from '../../../models/building.model';
 
 @Component({
   selector: 'app-listfloors',
   standalone: false,
   templateUrl: './listfloors.html',
-  styleUrl: './listfloors.css'
+  styleUrl: './listfloors.css',
 })
 export class Listfloors {
   floors!: any;
+  buildings!: Building[];
 
   constructor(
     private floorService: FloorService,
+    private buildingService: BuildingService,
     private router: Router,
     private cdr: ChangeDetectorRef
-  ) { }
+  ) {}
 
   ngOnInit(): void {
     this.listFloors();
+    this.listBuildings();
   }
 
   // Get List of Buildings
   listFloors(): void {
     this.floors = this.floorService.listFloors();
   }
+
+  // Get Building Name
+  getBuildingName(id: string): string {
+    const building = this.buildings.find((b) => b.id === id);
+    return building ? building.name : 'Unknown';
+  }
+
+  // Building List
+  listBuildings(): void {
+  this.buildingService.listBuildings().subscribe(
+    (data: Building[]) => {
+      this.buildings = data;
+    }
+  );
+}
 
   // View Building
   viewFloors(id: string): void {
@@ -46,7 +66,7 @@ export class Listfloors {
       },
       error: (error) => {
         console.error(error);
-      }
+      },
     });
   }
 }
