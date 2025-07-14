ee86b3011d18afb4823f8e556e26f38adc525694
Eiasin Prodhan <eiasinprodhan@gmail.com>
2025-07-15 01:03:59 +0600

    Added

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +10,17 @@ import { Component } from '@angular/core';
 })
 export class App {
   protected title = 'CREM-Angular';
+  showNavbar = true;
+
+  constructor(private router: Router) {
+    // Listen to router events
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        // List of routes where navbar should be hidden
+        const hiddenRoutes = ['/', '/signin', '/signup'];
+
+        this.showNavbar = !hiddenRoutes.includes(event.urlAfterRedirects);
+      });
+  }
 }
